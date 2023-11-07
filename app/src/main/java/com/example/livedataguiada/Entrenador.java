917d7a9b1ec327a734093480a704b770c4c2f1ba
laura1513 <laurajorques2003@gmail.com>
2023-11-07 17:14:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/livedataguiada/Entrenador.java
@@ -0,0 +1,44 @@
+package com.example.livedataguiada;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Entrenador {
+
+    interface EntrenadorListener {
+        void cuandoDeLaOrden(String orden);
+    }
+
+    Random random = new Random();
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    ScheduledFuture<?> entrenando;
+
+    void iniciarEntrenamiento(EntrenadorListener entrenadorListener) {
+        if (entrenando == null || entrenando.isCancelled()) {
+            entrenando = scheduler.scheduleAtFixedRate(new Runnable() {
+                int ejercicio;
+                int repeticiones = -1;
+
+                @Override
+                public void run() {
+                    if (repeticiones < 0) {
+                        repeticiones = random.nextInt(3) + 3;
+                        ejercicio = random.nextInt(5)+1;
+                    }
+                    entrenadorListener.cuandoDeLaOrden("EJERCICIO" + ejercicio + ":" + (repeticiones == 0 ? "CAMBIO" : repeticiones));
+                    repeticiones--;
+                }
+            }, 0, 1, SECONDS);
+        }
+    }
+
+    void pararEntrenamiento() {
+        if (entrenando != null) {
+            entrenando.cancel(true);
+        }
+    }
+}

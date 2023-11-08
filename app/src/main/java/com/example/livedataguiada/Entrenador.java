0207b0c8bd06532ba3a073b4f6036d6d705d648f
laura1513 <laurajorques2003@gmail.com>
2023-11-08 19:42:30 +0100

    Corregir Function, Juan va a mirarlo

--- app/src/main/java/com/example/livedataguiada/Entrenador.java
+++ app/src/main/java/com/example/livedataguiada/Entrenador.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ScheduledFuture;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+import androidx.lifecycle.LiveData;
+
 public class Entrenador {
 
     interface EntrenadorListener {
@@ -16,6 +18,27 @@ public class Entrenador {
     Random random = new Random();
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     ScheduledFuture<?> entrenando;
+
+    LiveData<String> ordenLiveData = new LiveData<String>() {
+        @Override
+        protected void onActive() {
+            super.onActive();
+
+            iniciarEntrenamiento(new EntrenadorListener() {
+                @Override
+                public void cuandoDeLaOrden(String orden) {
+                    postValue(orden);
+                }
+            });
+        }
+
+        @Override
+        protected void onInactive() {
+            super.onInactive();
+
+            pararEntrenamiento();
+        }
+    };
 
     void iniciarEntrenamiento(EntrenadorListener entrenadorListener) {
         if (entrenando == null || entrenando.isCancelled()) {
@@ -41,4 +64,5 @@ public class Entrenador {
             entrenando.cancel(true);
         }
     }
+
 }
